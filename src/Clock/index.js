4baefc30d0c609765829ee4ff6bfff7211e53589
Grzegorz Oleksa <grzesieq92@gmail.com>
2025-01-24 20:28:19 +0100

    showing date and time in clock

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -3,17 +3,17 @@ import "./style.css";
 import { useState } from "react";
 
 const Clock = () => {
-  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const [time, setTime] = useState(new Date());
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
+      setTime(new Date());
     }, 1000);
 
     return () => clearInterval(intervalId);
   }, []);
 
-  return <p className="clock">{time}</p>;
+  return <p className="clock">{time.toLocaleString()}</p>;
 };
 
 export default Clock;
