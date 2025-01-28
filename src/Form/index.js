475f2531f3399018b323b808b2d50d5aeb429732
Grzegorz Oleksa <grzesieq92@gmail.com>
2025-01-28 19:18:17 +0100

    added styled components to Form

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,42 +1,54 @@
-import "./style.css";
-import { StyledForm } from "./styled";
+import {
+  StyledForm,
+  Legend,
+  Input,
+  Select,
+  Button,
+  ResultView,
+} from "./styled";
 
-const Form = ({ amount, currency, result, handleAmountChange, handleCurrencyChange, onSubmit, onReset }) => (
-    <StyledForm onSubmit={onSubmit}>
-      <legend className="form__legend">Currency converter</legend>
-      <p>
-        <input
-          className="form__input"
-          type="number"
-          required
-          min="1"
-          step="any"
-          value={amount}
-          onChange={handleAmountChange}
-        />
-        PLN
-        <select
-          className="form__select"
-          name="currencies"
-          required
-          value={currency}
-          onChange={handleCurrencyChange}
-        >
-          <option value="">Select currencies</option>
-          <option value="EUR">EUR</option>
-          <option value="USD">USD</option>
-        </select>
-      </p>
-      <p>
-        <button type="submit" className="form__button">
-          Convert
-        </button>
-        <button type="button" className="form__button" onClick={onReset}>
-          Reset form
-        </button>
-      </p>
-      <p className="form__resultView">Result: <span className="form__result">{result}</span></p>
-    </StyledForm>
-  );
-  
-  export default Form;
+const Form = ({
+  amount,
+  currency,
+  result,
+  handleAmountChange,
+  handleCurrencyChange,
+  onSubmit,
+  onReset,
+}) => (
+  <StyledForm onSubmit={onSubmit}>
+    <Legend>Currency converter</Legend>
+    <p>
+      <Input
+        type="number"
+        required
+        min="1"
+        step="any"
+        value={amount}
+        onChange={handleAmountChange}
+      />
+      PLN
+      <Select
+        name="currencies"
+        required
+        value={currency}
+        onChange={handleCurrencyChange}
+      >
+        <option value="">Select currencies</option>
+        <option value="EUR">EUR</option>
+        <option value="USD">USD</option>
+      </Select>
+    </p>
+    <p>
+      <Button type="submit">Convert</Button>
+      <Button type="button" onClick={onReset}>
+        Reset form
+      </Button>
+    </p>
+    <ResultView>
+      Result: <span className="form__result">{result}</span>
+    </ResultView>
+  </StyledForm>
+);
+
+export default Form;
--- src/Form/styled.js
+++ src/Form/styled.js
@@ -11,4 +11,47 @@ export const StyledForm = styled.form`
   border-radius: 15px;
   text-align: center;
   border: 2px solid black;
-  margin-top: 20px;`;
+  margin-top: 20px;
+`;
+
+export const Legend = styled.legend`
+  font-style: oblique;
+  font-size: 40px;
+  background-color: rgb(255, 255, 255);
+  border-radius: 60%;
+  padding: 2px;
+  text-align: center;
+  border: 1px dotted;
+`;
+
+export const Input = styled.input`
+  padding: 5px;
+  margin: 5px;
+  border-radius: 5px;
+`;
+
+export const Select = styled.select`
+  padding: 5px;
+  margin: 5px;
+  border-radius: 5px;
+`;
+
+export const Button = styled.button`
+  padding: 10px;
+  margin-left: 15px;
+  background-color: hsl(118, 63%, 66%);
+  margin-top: 350px;
+  border-radius: 5px;
+
+  &:hover {
+    background-color: hsl(118, 63%, 80%);
+  }
+`;
+
+export const ResultView = styled.p`
+  background-color: rgb(180, 244, 182);
+  padding: 5px;
+  margin: 0 auto;
+  max-width: 50%;
+  font-size: large;
+  border: 1px solid black;`;
