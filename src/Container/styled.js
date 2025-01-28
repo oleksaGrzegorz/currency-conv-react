ae4ba8769229371a16fb58fe220fbe77dc72e7a4
Grzegorz Oleksa <grzesieq92@gmail.com>
2025-01-28 14:25:12 +0100

    added styled components to container

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -2,4 +2,11 @@ import styled from "styled-components";
 export const Wrapper = styled.main`
   margin-top: 100px;
   padding: 0px;
+
+  @media (max-width: 768px) {
+    margin-left: 10px;
+    margin-right: 10px;
+    min-height: 100vh;
+    margin-top: 150px;
+  }
 `;
