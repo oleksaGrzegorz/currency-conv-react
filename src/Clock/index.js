1e2b3f0416b2ec3d614dbd7d2b5fde0c4c120707
Grzegorz Oleksa <grzesieq92@gmail.com>
2025-01-28 10:05:01 +0100

    created custom hook "useCurrentDate"

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,29 +1,22 @@
-import { useEffect } from "react";
 import "./style.css";
-import { useState } from "react";
+import useCurrentDate from "./useCurrentDate";
+
+const formattedDate = (date) => date.toLocaleString("pl", {
+  weekday: "long",
+  day: "numeric",
+  month: "numeric",
+  year: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+});
 
 const Clock = () => {
-  const [time, setTime] = useState(new Date());
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-
-    return () => clearInterval(intervalId);
-  }, []);
+  const date = useCurrentDate();
 
   return (
     <p className="clock">
-      {time.toLocaleString("pl", {
-        weekday: "long",
-        day: "numeric",
-        month: "numeric",
-        year: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-      })}
+      {formattedDate(date)}
     </p>
   );
 };
