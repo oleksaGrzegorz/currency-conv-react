7cc4e3ecff2edff50d8999a0a5cc5f8cb5675830
Grzegorz Oleksa <grzesieq92@gmail.com>
2025-01-28 14:35:18 +0100

    code formatted

--- src/Clock/styled.js
+++ src/Clock/styled.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 export const ClockWithDate = styled.p`
-    text-align: center;
-    font-size: 30px;
-    padding-top: 0;
-    margin-top: 10px;
-    background-color: antiquewhite;
-    max-width: 42%;
-    margin: 0 auto;
-    border-radius: 50px;
-    border: 1px solid;
-    `;
+  text-align: center;
+  font-size: 30px;
+  padding-top: 0;
+  margin-top: 10px;
+  background-color: antiquewhite;
+  max-width: 42%;
+  margin: 0 auto;
+  border-radius: 50px;
+  border: 1px solid;
+`;
