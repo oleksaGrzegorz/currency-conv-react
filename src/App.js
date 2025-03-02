765288ce669b6bd59220b44bce6819ead8645e3b
Grzegorz Oleksa <grzesieq92@gmail.com>
2025-03-02 18:02:27 +0100

    5s -> 2s loading

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
           setLoading(false);
           setError("Wystąpił błąd podczas pobierania danych walutowych.");
         });
-    }, 5000);
+    }, 2000);
 
     return () => clearTimeout(currencyFetchTimer);
   }, []);
