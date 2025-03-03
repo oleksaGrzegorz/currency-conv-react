0304040396ab70a1e39e2f64dfbe0af6262e6622
Grzegorz Oleksa <grzesieq92@gmail.com>
2025-03-03 18:15:56 +0100

    added custom hook

--- src/App.js
+++ src/App.js
@@ -1,48 +1,16 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState } from "react";
 import "./index.css";
 import Container from "./Container";
 import Form from "./Form";
 import Clock from "./Clock";
+import useCurrencyRates from "./useCurrencyRates";
 
 function App() {
   const [amount, setAmount] = useState("");
   const [currency, setCurrency] = useState("");
   const [result, setResult] = useState("");
-  const [ratesData, setRatesData] = useState({
-    rates: {},
-    loading: true,
-    error: null,
-  });
 
-  useEffect(() => {
-    setRatesData({
-      rates: {},
-      loading: true,
-      error: null,
-    });
-
-    const currencyFetchTimer = setTimeout(() => {
-      axios
-        .get("/currencies.json")
-        .then((response) => {
-          setRatesData({
-            rates: response.data,
-            loading: false,
-            error: null,
-          });
-        })
-        .catch(() => {
-          setRatesData({
-            rates: {},
-            loading: false,
-            error: "Wystąpił błąd podczas pobierania danych walutowych.",
-          });
-        });
-    }, 2000);
-
-    return () => clearTimeout(currencyFetchTimer);
-  }, []);
+  const ratesData = useCurrencyRates();
 
   const calculateResult = (event) => {
     event.preventDefault();
@@ -51,7 +19,9 @@ function App() {
       return;
     }
     setResult(
-      `${amount} PLN = ${(amount / ratesData.rates[currency]).toFixed(2)} ${currency}`
+      `${amount} PLN = ${(amount / ratesData.rates[currency]).toFixed(
+        2
+      )} ${currency}`
     );
   };
 
